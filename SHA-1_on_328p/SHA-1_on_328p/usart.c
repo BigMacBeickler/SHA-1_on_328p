c48fc3a1165ba95668453badf67e9824ea5049b5
Christoph <info@beickler.org>
2025-02-24 19:32:23 +0100

    added header and datasheets

---
+++ SHA-1_on_328p/SHA-1_on_328p/usart.c
@@ -0,0 +1,6 @@
+/*
+ * usart.c
+ *
+ * Created: 24.02.2025 19:26:08
+ *  Author: BigMac
+ */ 
---
+++ SHA-1_on_328p/SHA-1_on_328p/usart.h
@@ -0,0 +1,16 @@
+/*
+ * usart.h
+ *
+ * Created: 24.02.2025 19:25:08
+ *  Author: BigMac
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+
+
+
+
+
+#endif /* USART_H_ */
